6c63e362ee10defef543b88c1f5539668cb079e3
hissyam90 <hissyam08@gmail.com>
2025-12-03 23:38:29 +0800

    refactor: fix project detail

--- src/App.jsx
+++ src/App.jsx
@@ -55,11 +55,11 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2025{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            EkiZR™
+            Antung Hissyam
           </a>
-          . All Rights Reserved.
+          <br></br> All Rights Reserved.
         </span>
       </center>
     </footer>
